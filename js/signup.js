1b97967fc397cf0f3264073ee9f63bf67010208a
iGuest <amaralli@uw.edi>
2014-11-25 16:49:16 -0800

    drgrgs

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,14 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+document.addEventListener('DOMContentLoaded', onLoad);
+
+function onLoad () {
+    Object.keys(usStates).forEach(function(element, index) {
+       var stateAbbrev =  document.createElement("OPTION");
+       document.getElementById("option").label = usStates[index].code;
+       document.getElementById("state").appendChild(stateAbbrev);
+    });
+
+}
